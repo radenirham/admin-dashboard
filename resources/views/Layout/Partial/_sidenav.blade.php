ebbefebd83a9cacfebd0c0a4bb050a0dd5b4bf4d
radenirham <44081911+radenirham@users.noreply.github.com>
2022-11-21 09:25:17 +0700

    Update _sidenav.blade.php

--- resources/views/Layout/Partial/_sidenav.blade.php
+++ resources/views/Layout/Partial/_sidenav.blade.php
@@ -1,7 +1,7 @@
 <!-- BEGIN: SideNav-->
 <aside class="sidenav-main nav-expanded nav-lock nav-collapsible sidenav-light navbar-full sidenav-active-rounded">
     <div class="brand-sidebar">
-        <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="index.html"><img src="{{asset('app-assets/images/logo/materialize-logo.png')}}" alt="materialize logo" /><span class="logo-text hide-on-med-and-down">Materialize</span></a><a class="navbar-toggler" href="#"><i class="material-icons">radio_button_checked</i></a></h1>
+        <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="{{url('/')}}"><img src="{{asset('app-assets/images/logo/materialize-logo.png')}}" alt="materialize logo" /><span class="logo-text hide-on-med-and-down">Materialize</span></a><a class="navbar-toggler" href="#"><i class="material-icons">radio_button_checked</i></a></h1>
     </div>
     <ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out" data-menu="menu-navigation" data-collapsible="menu-accordion">
         <li class="navigation-header"><a class="navigation-header-text">CRUD</a><i class="navigation-header-icon material-icons">more_horiz</i>
